b5a1e3549edf572d69a4e0db66c235e80b86cca6
YAWAL <yavorskyyval@gmail.com>
2017-06-15 22:46:36 +0300

    initial commit

---
+++ testGoglang/HelloWorlf.go
@@ -0,0 +1,5 @@
+package testGoglang
+
+func print(){
+	println("print this")
+}
---
+++ testGoglang/main/HelloWorld.go
@@ -0,0 +1,19 @@
+package main
+
+func main(){
+	println("hello world")
+	var a, f int
+	f = 4
+	a = f
+	//var world, phrase string
+	var world = "world"
+	var phrase = "phrase"
+
+	println( "a + f = " , f, a, a + f)
+	println(world + phrase)
+	f = 7
+	println("a + f = " , f, a , a + f)
+	kredo := "kredo"
+	println(kredo)
+}
+
