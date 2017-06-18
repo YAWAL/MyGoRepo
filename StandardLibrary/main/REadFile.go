024a5c590571f6384515854cc0618d526106c882
YAWAL <yavorskyyval@gmail.com>
2017-06-18 23:47:26 +0300

    training

---
+++ StandardLibrary/main/REadFile.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"os"
+	"fmt"
+)
+
+
+func main() {
+	file, err := os.Open("test.txt")
+	if err != nil {
+		// handle the error here
+		return
+	}
+	defer file.Close()
+
+	// get the file size
+	stat, err := file.Stat()
+	if err != nil {
+		return
+	}
+	// read the file
+	bs := make([]byte, stat.Size())
+	_, err = file.Read(bs)
+	if err != nil {
+		return
+	}
+
+	str := string(bs)
+	fmt.Println(str)
+}
---
+++ src/golang-book/chapter11/main.go
@@ -0,0 +1,12 @@
+package chapter11
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	xs := []float64{1,2,3,4}
+	avg := math.Average(xs)
+	fmt.Println(avg)
+}
