118a101e5afe6c75b6c3706304512616936d481d
YAWAL <yavorskyyval@gmail.com>
2017-07-03 19:49:17 +0300

    training

---
+++ Lv-252-Go/Week1/Task1/main/task107.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"math"
+	"fmt"
+)
+
+/*
+Дано ціле число m > 1. Отримати найбільше ціле k, при якому 4^k < m
+ */
+
+func showBiggestK( m int) {
+	if  isMmoreThan5(m){
+		fmt.Println("Biggest k value for equation 4^k < m , where m = ", m, " -> ", findMaxValueOfK(createSliceOfK(m)))
+	}
+}
+
+func isMmoreThan5(m int) bool {
+	if m > 5 {
+		return true
+	}
+	fmt.Println("There are no k where equation 4^k < m is true")
+	return false
+}
+
+func createSliceOfK(m int) []int{
+	//empty slice for allocation values of k with capacity m (to prevent runtime error -> panic: runtime error: index out of range)
+	valuesOfK :=make([]int, m)
+	//creating slice of k
+	for k := 1; int(math.Pow(4, float64(k))) < m; k++{
+		for j := k - 1; j < k ; j++ {
+			valuesOfK[j] = k
+		}
+	}
+	return valuesOfK
+}
+
+func findMaxValueOfK(valuesOfK []int) int {
+	max := valuesOfK[0]
+	for _, next := range valuesOfK {
+		if max < next  {
+			max = next
+		}
+	}
+	return max
+}
+
+func main()  {
+
+	showBiggestK(17)
+
+}
---
+++ Lv-252-Go/Week1/Task2/main/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+//answer for task 243б page 104
+func twoRootsFor243(n int)  {
+	var x, y int
+	for x = 1; x < n; x ++ {
+		for y = 1; y < n; y ++  {
+			if x*x + y*y == n {
+				//fmt.Println("x*x + y*y = n" , x, y ,n)
+				fmt.Println("twoRootsFor243:Pair of natural numbers when x*x + y*y = n : x =", x, " y =", y, " n = ", n)
+			}
+		}
+	}
+}
+
+//answer for task 243а page 104
+func oneRootFor243(n int)  {
+	var x int
+	//when one root (y) member already defined (y = 1)
+	y := 1
+	for x = 1; x < n; x ++ {
+		if x*x + y*y == n {
+			//fmt.Println("x*x + y*y = n" , x, y ,n)
+			fmt.Println("oneRootFor243:Pair of natural numbers when x*x + y*y = n : x =", x, " y =", y, " n = ", n)
+		}
+	}
+}
+func main()  {
+	oneRootFor243(10)
+	twoRootsFor243(10)
+}
