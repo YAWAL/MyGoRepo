bd5a110c87cd84d64ccc5c322ac07675bdf20d2c
YAWAL <yavorskyyval@gmail.com>
2017-07-18 17:44:27 +0300

    training

--- Pointers/main/Pointer1.go
+++ Pointers/main/Pointer1.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+type Saiyan struct {
+	Name string
+	Power int
+}
+
 func zero(xPtr *int) {
 	*xPtr = 0
 }
@@ -9,6 +14,11 @@ func main() {
 	x := 5
 	zero(&x)
 	fmt.Println(x) // x is 0
+
+
+	goku := &Saiyan{"Goku", 9000}
+	Super(goku)
+	fmt.Println(goku.Power)
 }
 /*
 Указатели указывают на участок в памяти, где хранится значение.
@@ -19,3 +29,6 @@ func main() {
  оператор &, который используется для получения адреса переменной. &x вернет *int (указатель на int)
   потому что x имеет тип int.
  */
+func Super(s *Saiyan) {
+	s = &Saiyan{"Gohan", 1000}
+}
