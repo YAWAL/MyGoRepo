c19615d3f48f3eb59dc65c634185ac27dc55443c
YAWAL <yavorskyyval@gmail.com>
2017-07-22 12:33:09 +0300

    training

--- DataBases/sqlite/main/sqlite.go
+++ DataBases/sqlite/main/sqlite.go
@@ -1,8 +1,20 @@
 package main
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+)
 
 var (
 	db, _    = sql.Open("sqlite3", "DataBase/sqlite/client_db")
-	createDB = "create table if not exists contacts (id primary key int, name varchar (20), birthday date, isblocked boolean)"
+	createDBquery = "create table if not exists contacts (id primary key int, name varchar (20), birthday date, isblocked boolean)"
 )
+
+func createDB(query string) error {
+	result, err := db.Exec(createDBquery)
+	if err != nil {
+		return err
+	}
+	fmt.Print(result)
+	return nil
+}
