39a4c4ac3c866de5a8444c62589d75fbd24fcfb7
YAWAL <yavorskyyval@gmail.com>
2017-07-22 13:10:51 +0300

    training

--- DataBases/sqlite/main/sqlite.go
+++ DataBases/sqlite/main/sqlite.go
@@ -10,11 +10,41 @@ var (
 	createDBquery = "create table if not exists contacts (id primary key int, name varchar (20), birthday date, isblocked boolean)"
 )
 
+type Contact struct {
+	Id int
+	Name string
+	Birtday int
+	IsBlocked bool
+}
+
 func createDB(query string) error {
 	result, err := db.Exec(createDBquery)
 	if err != nil {
 		return err
 	}
+
+
 	fmt.Print(result)
 	return nil
+}
+
+func (c *Contact) InsertIntoContact(Id int, Name string, Birtday int, IsBlocked bool ) (bool, error) {
+
+	tx, _ := db.Begin()
+	stmp, _ := tx.Prepare("insert into contacts(id, name, birthday, isblocked) values (?, ?, ?)")
+
+	_, err := stmp.Exec(c.Id, c.Birtday, c.IsBlocked)
+
+	tx.Commit()
+
+	return true, err
+
+}
+
+func main()  {
+	createDB(createDBquery)
+	c := Contact{1,"sdfdsf",1568904576,false}
+	c.InsertIntoContact(56,"56767",1568904576,true)
+
+
 }
