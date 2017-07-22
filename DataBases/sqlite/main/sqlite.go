970488d2fbffcd3fcab513735a91795eeaff60da
YAWAL <yavorskyyval@gmail.com>
2017-07-22 19:54:02 +0300

    training

--- DataBases/sqlite/main/sqlite.go
+++ DataBases/sqlite/main/sqlite.go
@@ -3,10 +3,12 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 var (
-	db, _         = sql.Open("sqlite3", "DataBase/sqlite/client_db")
+	db, _ = sql.Open("sqlite3", "DataBase/sqlite/client_db")
+
 	createDBquery = "create table if not exists contacts (id primary key int, name varchar (20), birthday date, isblocked boolean)"
 )
 
@@ -18,6 +20,7 @@ type Contact struct {
 }
 
 func createDB(query string) error {
+
 	result, err := db.Exec(createDBquery)
 	if err != nil {
 		return err
@@ -42,7 +45,7 @@ func (c *Contact) InsertIntoContact(Id int, Name string, Birtday int, IsBlocked 
 
 func main() {
 	//createDB(createDBquery)
-	c := Contact{}
-	c.InsertIntoContact(56, "56767", 1568904576, true)
+	//c := Contact{}
+	//c.InsertIntoContact(56, "56767", 1568904576, true)
 
 }
