bfb60a267ae3b64e50ebf8958e6d61b9cb9b2e56
YAWAL <yavorskyyval@gmail.com>
2017-07-22 12:15:11 +0300

    training

--- DataBases/sqlite/main/sqlite.go
+++ DataBases/sqlite/main/sqlite.go
@@ -2,4 +2,7 @@ package main
 
 import "database/sql"
 
-var db, _ = sql.Open("sqlite3","DataBase/sqlite/client_db")
+var (
+	db, _    = sql.Open("sqlite3", "DataBase/sqlite/client_db")
+	createDB = "create table if not exists contacts (id primary key int, name varchar (20), birthday date, isblocked boolean)"
+)
