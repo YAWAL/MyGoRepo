be09f5f3c6f5b14996a53c285131d117b975b0fb
YAWAL <yavorskyyval@gmail.com>
2017-07-23 13:14:01 +0300

    training

--- DataBases/MySQL/main/MySQGORM.go
+++ DataBases/MySQL/main/MySQGORM.go
@@ -3,32 +3,38 @@ package main
 import (
 	"github.com/jinzhu/gorm"
 	//"github.com/ziutek/mymysql/mysql"
-	//_ "github.com/ziutek/mymysql/native"  // Native engine
-	//_ "github.com/ziutek/mymysql/thrsafe" // Thread safe engine
+	_ "github.com/ziutek/mymysql/native"  // Native engine
+	_ "github.com/ziutek/mymysql/thrsafe" // Thread safe engine
 	"fmt"
 )
 
-
 var (
 	address = "127.0.0.1:3306"
-	user = "root"
-	pass = "root"
-	dbname = "users"
+	user    = "root"
+	pass    = "root"
+	dbname  = "users"
 )
 
+func connectDB() *gorm.DB {
+	db, err := gorm.Open("mysql", "root@/users?charset=utf8&parseTime=True&loc=Local")
+	db.LogMode(true)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return db
+}
 
 func main() {
+	db, err := connectDB()
+	//db, err := gorm.Open("MySQL", "tcp", "", address, user, pass, dbname)
+	//
+	//if err != nil {
+	//	panic(err.Error())
+	//}
 
-
-	db, err := gorm.Open("MySQL","tcp", "", address, user, pass, dbname )
-
-	if err != nil {
-		panic(err.Error())
-	}
-
-	err = db.DB().Ping()
+	err := connectDB().DB().Ping()
 
 	fmt.Println(err)
 
-	db.Close()
+	//db.Close()
 }
