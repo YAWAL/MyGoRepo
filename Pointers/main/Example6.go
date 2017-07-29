f9a7d90587aee9b17e32d895e05d39ebfc0d3b0c
YAWAL <yavorskyyval@gmail.com>
2017-07-29 17:00:41 +0300

    training

--- Pointers/main/Example6.go
+++ Pointers/main/Example6.go
@@ -10,10 +10,18 @@ func modify2(arr *[3]int) {
 	arr[0] = 90
 }
 
+func modify3(sls []int) {
+	sls[0] = 90
+}
+
 func main() {
 	a := [3]int{89, 90, 91}
 	modify1(&a)
 	fmt.Println(a)
+
 	modify2(&a)
+	fmt.Println(a)
+
+	modify3(a[:])
 	fmt.Println(a)
 }
