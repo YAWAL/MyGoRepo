395f11c6339d92e4e32405faa2595a259a6a7d26
YAWAL <yavorskyyval@gmail.com>
2017-08-02 22:32:31 +0300

    training

--- chat/main/main.go
+++ chat/main/main.go
@@ -3,7 +3,26 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync"
+	"path/filepath"
+	"text/template"
 )
+
+// templ represents a single template
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServeHTTP handles the HTTP request.
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates",
+			t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
