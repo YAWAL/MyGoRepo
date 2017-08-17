6a4d617de979a2ae5c1821311aa16d6f1d06bef2
YAWAL <yavorskyyval@gmail.com>
2017-08-17 17:37:12 +0300

    training

---
+++ WorkWithStrings/main/containsAny.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println(strings.ContainsAny("Australia", "a"))
+	fmt.Println(strings.ContainsAny("Australia", "r & a"))
+	fmt.Println(strings.ContainsAny("JAPAN", "j"))
+	fmt.Println(strings.ContainsAny("JAPAN", "J"))
+	fmt.Println(strings.ContainsAny("JAPAN", "JAPAN"))
+	fmt.Println(strings.ContainsAny("JAPAN", "japan"))
+	fmt.Println(strings.ContainsAny("Shell-12541", "1"))
+
+	//  Contains vs ContainsAny
+	fmt.Println(strings.ContainsAny("Shell-12541", "1-2")) // true
+	fmt.Println(strings.Contains("Shell-12541", "1-2"))   // false
+}
