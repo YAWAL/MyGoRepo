083efc3116dcb0a013f9aad412c3f8cd000608dd
YAWAL <yavorskyyval@gmail.com>
2017-08-26 21:46:50 +0300

    training

--- DataBases/redis/radix/main/radixChecking.go
+++ DataBases/redis/radix/main/radixChecking.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"github.com/mediocregopher/radix.v2/pool"
+	"github.com/8tomat8/SSU-Golang-252-Chat/loger"
+)
+
+func main() {
+	p, err := pool.New("tcp", "localhost:6379", 10)
+	if err != nil {
+		loger.Log.Error(err)
+	}
+	loger.Log.Print("pool created")
+
+	// In another go-routine
+
+	conn, err := p.Get()
+	if err != nil {
+		loger.Log.Error(err)
+	}
+	loger.Log.Print("connection established")
+
+
+	if conn.Cmd("SOME", "CMD").Err != nil {
+		loger.Log.Errorf("error : ", err)
+	}
+
+	p.Put(conn)}
