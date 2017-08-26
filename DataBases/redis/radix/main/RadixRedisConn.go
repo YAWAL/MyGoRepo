52d81eafcdba7b0757840ec76ef4d2db3762a224
YAWAL <yavorskyyval@gmail.com>
2017-08-26 23:58:25 +0300

    training

---
+++ DataBases/redis/radix/main/RadixRedisConn.go
@@ -0,0 +1,26 @@
+package main
+
+import "github.com/mediocregopher/radix.v2/pool"
+import "github.com/mediocregopher/radix.v2/redis"
+import "github.com/8tomat8/SSU-Golang-252-Chat/loger"
+
+
+const NETWORK = "tcp"
+const ADDRESS = "localhost:6379"
+const POOL_SIZE = 10
+
+func GetRedisConn(network string, addr string, size int) (conn *redis.Client, err error) {
+	p, err := pool.New(network, addr, size)
+	if err != nil {
+		loger.Log.Error(err)
+	}
+	loger.Log.Print("pool created")
+	conn, err = p.Get()
+	if err != nil {
+		loger.Log.Error(err)
+	}
+	loger.Log.Print("connection established")
+	//resp := conn.Cmd("ping")
+	//fmt.Printf("resi value is %v , type is %T \n", resp, resp )
+	return conn, err
+}
--- DataBases/redis/radix/main/radixChecking.go
+++ DataBases/redis/radix/main/radixChecking.go
@@ -5,12 +5,8 @@ import (
 	"github.com/8tomat8/SSU-Golang-252-Chat/loger"
 	"fmt"
 	"os"
-	"github.com/mediocregopher/radix.v2/redis"
 )
 
-const NETWORK  = "tcp"
-const ADDRESS  = "localhost:6379"
-const POOL_SIZE  = 10
 
 
 func errHndlr(err error) {
@@ -20,21 +16,7 @@ func errHndlr(err error) {
 	}
 }
 
-func GetRedisConn(network string, addr string, size int) (conn *redis.Client, err error) {
-	p, err := pool.New(network, addr , size)
-	if err != nil {
-		loger.Log.Error(err)
-	}
-	loger.Log.Print("pool created")
-	conn, err = p.Get()
-	if err != nil {
-		loger.Log.Error(err)
-	}
-	loger.Log.Print("connection established")
-	//resp := conn.Cmd("ping")
-	//fmt.Printf("resi value is %v , type is %T \n", resp, resp )
-	return conn, err
-}
+
 
 func main() {
 	conn, err := GetRedisConn(NETWORK, ADDRESS, POOL_SIZE)
@@ -51,6 +33,10 @@ func main() {
 
 	r = conn.Cmd("set", "mykey0", "myval0")
 	errHndlr(r.Err)
+
+	s, err := conn.Cmd("get", "mykey0").Str()
+	errHndlr(err)
+	fmt.Println("mykey0:", s)
 
 
 }
