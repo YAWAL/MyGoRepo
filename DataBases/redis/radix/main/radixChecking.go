1b6142318ede444b6419f9978ecf9adf21003092
YAWAL <yavorskyyval@gmail.com>
2017-08-26 22:56:39 +0300

    training

--- DataBases/redis/radix/main/radixChecking.go
+++ DataBases/redis/radix/main/radixChecking.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/mediocregopher/radix.v2/pool"
 	"github.com/8tomat8/SSU-Golang-252-Chat/loger"
+	"fmt"
 )
 
 func main() {
@@ -21,8 +22,18 @@ func main() {
 	loger.Log.Print("connection established")
 
 
+	resp := conn.Cmd("ping")
+	fmt.Printf("resi value is %v , type is %T", resp, resp )
+
+	if conn.Cmd("SADD", "myset", "some").Err != nil {
+		loger.Log.Errorf("error : ", err)
+
+	}
+
 	if conn.Cmd("SOME", "CMD").Err != nil {
 		loger.Log.Errorf("error : ", err)
 	}
 
-	p.Put(conn)}
+	p.Put(conn)
+
+}
