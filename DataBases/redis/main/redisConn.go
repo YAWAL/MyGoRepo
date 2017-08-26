c3cfac56762810ddcc262e4f996c2540029d8107
YAWAL <yavorskyyval@gmail.com>
2017-08-26 17:26:14 +0300

    training

--- DataBases/redis/main/redisConn.go
+++ DataBases/redis/main/redisConn.go
@@ -1,7 +1,27 @@
 package main
 
-import "github.com/garyburd/redigo/redis"
+import (
+	"github.com/garyburd/redigo/redis"
+	"fmt"
+)
 
 func GetRedisConnection() (redis.Conn, error) {
 	return redis.Dial("tcp", ":6379")
+}
+func main() {
+	connection,_ := dial()
+	defer connection.Close()
+	data, err := redis.Values(connection.Do("SORT", "Users", "BY", "User:*->name","GET", "User:*->name"))
+	if (err) {
+		fmt.Println("Error getting values", err)
+	}
+	for i:= range data {
+		var Uname string
+		data,err := redis.Scan(data, &Uname)
+		if (err) {
+			fmt.Println("Error getting value",err)
+		}else {
+			fmt.Println("Name Uname")
+		}
+	}
 }
