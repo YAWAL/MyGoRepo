ce59f99b7f1743b4d1cfe6566bddb5f353a84045
YAWAL <yavorskyyval@gmail.com>
2017-08-26 23:09:34 +0300

    training

--- DataBases/redis/radix/main/radixChecking.go
+++ DataBases/redis/radix/main/radixChecking.go
@@ -4,7 +4,15 @@ import (
 	"github.com/mediocregopher/radix.v2/pool"
 	"github.com/8tomat8/SSU-Golang-252-Chat/loger"
 	"fmt"
+	"os"
 )
+
+func errHndlr(err error) {
+	if err != nil {
+		fmt.Println("error:", err)
+		os.Exit(1)
+	}
+}
 
 func main() {
 	p, err := pool.New("tcp", "localhost:6379", 10)
@@ -13,27 +21,24 @@ func main() {
 	}
 	loger.Log.Print("pool created")
 
-	// In another go-routine
-
 	conn, err := p.Get()
 	if err != nil {
 		loger.Log.Error(err)
 	}
 	loger.Log.Print("connection established")
 
-
 	resp := conn.Cmd("ping")
-	fmt.Printf("resi value is %v , type is %T", resp, resp )
+	fmt.Printf("resi value is %v , type is %T \n", resp, resp )
 
-	if conn.Cmd("SADD", "myset", "some").Err != nil {
-		loger.Log.Errorf("error : ", err)
+	// select database
+	r := conn.Cmd("select", 0)
+	errHndlr(r.Err)
 
-	}
+	r = conn.Cmd("echo", "Hello world!")
+	errHndlr(r.Err)
 
-	if conn.Cmd("SOME", "CMD").Err != nil {
-		loger.Log.Errorf("error : ", err)
-	}
+	r = conn.Cmd("set", "mykey0", "myval0")
+	errHndlr(r.Err)
 
-	p.Put(conn)
 
 }
